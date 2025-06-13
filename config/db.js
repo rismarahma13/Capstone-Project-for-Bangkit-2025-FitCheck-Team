ade620190ce98eef900871ee4c663ad49eb6fc94
rismarahma13 <rismarahmatulummah@gmail.com>
2025-06-13 12:59:15 +0700

    Update db config and assets, remove old database.js

--- config/db.js
+++ config/db.js
@@ -1,23 +1,23 @@
-// FITCHECK/config/db.js
 const mysql = require("mysql2/promise"); // Menggunakan promise API untuk async/await
 
 const connectDB = async () => {
   try {
     const pool = mysql.createPool({
       host: process.env.DB_HOST,
+      port: process.env.DB_PORT, // tambahkan ini
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       waitForConnections: true,
-      connectionLimit: 10, // Jumlah maksimum koneksi dalam pool
-      queueLimit: 0, // Batas antrian untuk koneksi
+      connectionLimit: 10,
+      queueLimit: 0,
     });
 
-    console.log("MySQL Connected successfully");
-    return pool; // Mengembalikan pool koneksi untuk digunakan di aplikasi
+    console.log("✅ MySQL Connected successfully");
+    return pool;
   } catch (err) {
-    console.error("MySQL connection error:", err.message);
-    process.exit(1); // Keluar dari proses Node.js jika koneksi database gagal
+    console.error("❌ MySQL connection error:", err.message);
+    process.exit(1);
   }
 };
 
